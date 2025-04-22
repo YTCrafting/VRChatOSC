6c4e143d096bdaab73b3f5fe130604591e275758
YTCrafting <136029379+YTCrafting@users.noreply.github.com>
2025-04-22 02:12:12 +0200

    added movement methods

--- src/vrchatosc/vrchatosc.py
+++ src/vrchatosc/vrchatosc.py
@@ -1,4 +1,6 @@
 from pythonosc.udp_client import SimpleUDPClient
+from typing import Union
+import time
 
 class VRChatOSC:
     def __init__(self, ip:str="127.0.0.1", port:int=9000) -> None:
@@ -39,3 +41,55 @@ class VRChatOSC:
         self.client.send_message("input/QuickMenuToggleRight", 0)
         self.client.send_message("input/QuickMenuToggleRight", 1)
         self.client.send_message("input/QuickMenuToggleRight", 0)
+
+    def move_forward(self, value:Union[bool,int]) -> None:
+        """Moves the local VRChat player forwards.
+
+        Args:
+            value (bool or int, optional): Amount of seconds as integer or change forward walking state with bool.
+        """
+        if isinstance(value, bool):
+            self.client.send_message("/input/MoveForward", value)
+        elif isinstance(value, int):
+            self.client.send_message("/input/MoveForward", True)
+            time.sleep(value)
+            self.client.send_message("/input/MoveForward", False)
+
+    def move_backward(self, value:Union[bool,int]) -> None:
+        """Moves the local VRChat player backwards.
+
+        Args:
+            value (bool or int, optional): Amount of seconds as integer or change backward walking state with bool.
+        """
+        if isinstance(value, bool):
+            self.client.send_message("/input/MoveBackward", value)
+        elif isinstance(value, int):
+            self.client.send_message("/input/MoveBackward", True)
+            time.sleep(value)
+            self.client.send_message("/input/MoveBackward", False)
+
+    def move_left(self, value:Union[bool,int]) -> None:
+        """Moves the local VRChat player left.
+
+        Args:
+            value (bool or int): Amount of seconds as integer or change left walking state with bool.
+        """
+        if isinstance(value, bool):
+            self.client.send_message("/input/MoveLeft", value)
+        elif isinstance(value, int):
+            self.client.send_message("/input/MoveLeft", True)
+            time.sleep(value)
+            self.client.send_message("/input/MoveLeft", False)
+
+    def move_right(self, value:Union[bool,int]) -> None:
+        """Moves the local VRChat player right.
+
+        Args:
+            value (bool or int): Amount of seconds as integer or change right walking state with bool.
+        """
+        if isinstance(value, bool):
+            self.client.send_message("/input/MoveRight", value)
+        elif isinstance(value, int):
+            self.client.send_message("/input/MoveRight", True)
+            time.sleep(value)
+            self.client.send_message("/input/MoveRight", False)
