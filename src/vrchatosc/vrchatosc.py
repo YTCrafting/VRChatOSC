796c153d6b9df9caea15861a3bf94dca5a29b46d
YTCrafting <136029379+YTCrafting@users.noreply.github.com>
2025-04-23 15:33:22 +0200

    changed int to float

--- src/vrchatosc/vrchatosc.py
+++ src/vrchatosc/vrchatosc.py
@@ -42,54 +42,54 @@ class VRChatOSC:
         self.client.send_message("input/QuickMenuToggleRight", 1)
         self.client.send_message("input/QuickMenuToggleRight", 0)
 
-    def move_forward(self, value:Union[bool,int]) -> None:
+    def move_forward(self, value:Union[bool,float]) -> None:
         """Moves the local VRChat player forwards.
 
         Args:
-            value (bool or int, optional): Amount of seconds as integer or change forward walking state with bool.
+            value (bool or float): Amount of seconds as float or change forward walking state with bool.
         """
         if isinstance(value, bool):
             self.client.send_message("/input/MoveForward", value)
-        elif isinstance(value, int):
+        elif isinstance(value, float):
             self.client.send_message("/input/MoveForward", True)
             time.sleep(value)
             self.client.send_message("/input/MoveForward", False)
 
-    def move_backward(self, value:Union[bool,int]) -> None:
+    def move_backward(self, value:Union[bool,float]) -> None:
         """Moves the local VRChat player backwards.
 
         Args:
-            value (bool or int, optional): Amount of seconds as integer or change backward walking state with bool.
+            value (bool or float): Amount of seconds as float or change backward walking state with bool.
         """
         if isinstance(value, bool):
             self.client.send_message("/input/MoveBackward", value)
-        elif isinstance(value, int):
+        elif isinstance(value, float):
             self.client.send_message("/input/MoveBackward", True)
             time.sleep(value)
             self.client.send_message("/input/MoveBackward", False)
 
-    def move_left(self, value:Union[bool,int]) -> None:
+    def move_left(self, value:Union[bool,float]) -> None:
         """Moves the local VRChat player left.
 
         Args:
-            value (bool or int): Amount of seconds as integer or change left walking state with bool.
+            value (bool or float): Amount of seconds as float or change left walking state with bool.
         """
         if isinstance(value, bool):
             self.client.send_message("/input/MoveLeft", value)
-        elif isinstance(value, int):
+        elif isinstance(value, float):
             self.client.send_message("/input/MoveLeft", True)
             time.sleep(value)
             self.client.send_message("/input/MoveLeft", False)
 
-    def move_right(self, value:Union[bool,int]) -> None:
+    def move_right(self, value:Union[bool,float]) -> None:
         """Moves the local VRChat player right.
 
         Args:
-            value (bool or int): Amount of seconds as integer or change right walking state with bool.
+            value (bool or float): Amount of seconds as float or change right walking state with bool.
         """
         if isinstance(value, bool):
             self.client.send_message("/input/MoveRight", value)
-        elif isinstance(value, int):
+        elif isinstance(value, float):
             self.client.send_message("/input/MoveRight", True)
             time.sleep(value)
             self.client.send_message("/input/MoveRight", False)
@@ -100,28 +100,28 @@ class VRChatOSC:
         time.sleep(0.01)
         self.client.send_message("/input/Jump", False)
 
-    def look_left(self, value:Union[bool,int]) -> None:
+    def look_left(self, value:Union[bool,float]) -> None:
         """Turns local VRChat player anti-clockwise.
 
         Args:
-            value (bool or int): Amount of seconds as integer or change left turning state with bool.
+            value (bool or float): Amount of seconds as float or change left turning state with bool.
         """
         if isinstance(value, bool):
             self.client.send_message("/input/LookLeft", value)
-        elif isinstance(value, int):
+        elif isinstance(value, float):
             self.client.send_message("/input/LookLeft", True)
             time.sleep(value)
             self.client.send_message("/input/LookLeft", False)
 
-    def look_right(self, value:Union[bool,int]) -> None:
+    def look_right(self, value:Union[bool,float]) -> None:
         """Turns local VRChat player clockwise.
 
         Args:
-            value (bool or int): Amount of seconds as integer or change right turning state with bool.
+            value (bool or float): Amount of seconds as float or change right turning state with bool.
         """
         if isinstance(value, bool):
             self.client.send_message("/input/LookRight", value)
-        elif isinstance(value, int):
+        elif isinstance(value, float):
             self.client.send_message("/input/LookRight", True)
             time.sleep(value)
             self.client.send_message("/input/LookRight", False)
